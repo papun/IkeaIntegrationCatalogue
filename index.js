6d95beb092fb3cfcbabbe0a7eee343d587e1d4c0
papun <papun.sahoo@gmail.com>
2020-01-25 19:00:58 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 app.get("/", function (req, res) {
 	res.render("index");
 });
@@ -41,6 +42,8 @@ app.post('/searchresults', function (req, res) {
 	// res.render('results', { title: id });
 
 });
+
+
 app.listen(3000, function () {
 	console.log("server is listening!!!");
 });
